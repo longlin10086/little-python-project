b9e6b4ec9e67fc0d33ef17eedddb86c54d8be5dc
longlin <1031524609@qq.com>
2023-01-20 22:36:08 +0800

    更新us_state,添加missing_state.csv

--- us_state/main.py
+++ us_state/main.py
@@ -8,6 +8,8 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 data_primary = pandas.read_csv("50_states.csv")
 state_list = data_primary["state"].to_list()
+state_list_ignore = []
+answer = []
 state = ShowState()
 
 screen.addshape(image)
@@ -15,13 +17,21 @@ turtle.shape(image)
 
 is_on_game = True
 while is_on_game:
-    answer_state = screen.textinput(title="Guess the State", prompt=f"What's another state's name?({len(state.state_list)}/50)")
+    answer_state = screen.textinput(title="Guess the State",
+                                    prompt=f"What's another state's name?({len(state.state_list)}/50)").title()
     if answer_state in state_list:
+        answer.append(answer_state)
         x = int(data_primary[data_primary['state'] == answer_state].x)
         y = int(data_primary[data_primary['state'] == answer_state].y)
         position = (x, y)
         state.creat_state(position=position, name=answer_state)
-    if answer_state[0] == "q":
+    if answer_state[0] == "Q" or len(state.state_list) >= 50:
         is_on_game = False
 
+for item in state_list:
+    if item not in answer:
+        state_list_ignore.append(item)
+
+df = pandas.DataFrame(state_list_ignore)
+df.to_csv("missing_state.csv")
 turtle.exitonclick()
