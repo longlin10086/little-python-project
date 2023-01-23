11c7fc800177a0cafcae7c8ee8a9a390cbf45c4a
longlin <1031524609@qq.com>
2023-01-24 01:05:49 +0800

    添加pomodoro番茄钟项目

---
+++ pomodoro/main.py
@@ -0,0 +1,93 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+my_timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global my_timer
+    global reps
+    window.after_cancel(my_timer)
+    timer.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+    if reps % 2 == 1:
+        timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    elif reps % 8 == 0:
+        timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    else:
+        timer.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global reps
+    count_min = count // 60
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global my_timer
+        my_timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            check_marks.config(text="✔"*(reps//2))
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
+timer.grid(column=1, row=0)
+
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=photo)
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.grid(column=1, row=1)
+
+
+start = Button(text="Start", highlightthickness=0, command=start_timer)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset.grid(column=2, row=2)
+
+
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
