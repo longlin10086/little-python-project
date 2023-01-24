e23c06a8e0013d067778f6e0ee4b3bbdbb9b0801
longlin <1031524609@qq.com>
2023-01-24 15:18:00 +0800

    更新NATO_letter

--- NATO_letter/main.py
+++ NATO_letter/main.py
@@ -3,10 +3,20 @@ import pandas
 dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 dictionary = {row.letter: row.code for (index, row) in dataframe.iterrows()}
+is_on = True
+word_list = []
+while is_on:
+    user_name = input("Please input the word you want to convert: ").upper()
+    is_on = False
+    for letter in user_name:
+        try:
+            word_list.append(dictionary[letter])
+        except KeyError:
+            print("Please input a word whose letters are all in alphabet")
+            is_on = True
+            break
 
-user_name = input("Please input the word you want to convert: ").upper()
-for letter in user_name:
-    print(dictionary[letter])
+print(word_list)
 # for (index, row) in student_data_frame.iterrows():
 
 # Keyword Method with iterrows()
--- NATO_letter/playground.py
+++
@@ -1,5 +0,0 @@
-def add(*args):
-    return sum(args)
-
-
-print(add(1, 2, 3, 4, 5))
