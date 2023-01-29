c23edb6932a03df4a93f7ba690a4acf7d7502bd6
longlin <1031524609@qq.com>
2023-01-29 16:14:25 +0800

    增添quiz的again功能

--- quizzler_app/data.py
+++ quizzler_app/data.py
@@ -1,13 +1,14 @@
 import requests
 
-parameters = {
-    "amount": 10,
-    "type": "boolean",
-}
 
-url = "https://opentdb.com/api.php"
+def get_url():
+    parameters = {
+        "amount": 10,
+        "type": "boolean",
+    }
 
-response = requests.get(url=url, params=parameters)
-response.raise_for_status()
-question_data = response.json()
+    url = "https://opentdb.com/api.php"
 
+    response = requests.get(url=url, params=parameters)
+    response.raise_for_status()
+    return response.json()
--- quizzler_app/main.py
+++ quizzler_app/main.py
@@ -1,21 +1,21 @@
 from question_model import Question
-from data import question_data
+from data import get_url
 from quiz_brain import QuizBrain
 from ui import QuizInterface
+from tkinter import messagebox
 
-question_bank = []
-for question in question_data["results"]:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
-
-
-quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface(quiz)
-
-# while quiz.still_has_questions():
-#     quiz.next_question()
+is_on = True
+while is_on:
+    question_bank = []
+    question_data = get_url()
+    for question in question_data["results"]:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
 
 
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+    quiz = QuizBrain(question_bank)
+    quiz_ui = QuizInterface(quiz)
+
+    is_on = messagebox.askyesno(title="choose", message="Do you want to play a new game?")
